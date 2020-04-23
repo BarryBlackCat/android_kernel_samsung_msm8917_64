ce614b05a98f20215e6a04b9f6b1969c907b56c9
BarryBlackCat <everydayonechoice@gmail.com>
2020-04-23 15:29:14 +0000

    Update build script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-export ARCH=arm
-export PATH=$(pwd)/../PLATFORM/prebuilts/gcc/linux-x86/arm/arm-linux-androideabi-4.9/bin:$PATH
 
-mkdir out
+OUT_DIR=out
 
-make -C $(pwd) O=out CROSS_COMPILE=arm-linux-androideabi- j4primelte_sea_open_defconfig
-make -j64 -C $(pwd) O=out CROSS_COMPILE=arm-linux-androideabi-
- 
-cp out/arch/arm/boot/zImage $(pwd)/arch/arm/boot/zImage
+# you should change the "CROSS_COMPILE" to right toolchain path (you downloaded)
+# ex)CROSS_COMPILE={android platform directory you downloaded}/android/prebuilt/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+COMMON_ARGS="-C $(pwd) O=$(pwd)/${OUT_DIR} ARCH=arm CROSS_COMPILE=aarch64-linux-android- KCFLAGS=-mno-android"
+
+export PATH=/home/cranberry/Android-kernel-building/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin:$PATH
+export ARCH=arm64
+
+[ -d ${OUT_DIR} ] && rm -rf ${OUT_DIR}
+mkdir ${OUT_DIR}
+
+make ${COMMON_ARGS} j6primelte_defconfig
+make -j64 ${COMMON_ARGS}
+
+cp ${OUT_DIR}/arch/arm64/boot/zImage $(pwd)/arch/arm64/boot/zImage
